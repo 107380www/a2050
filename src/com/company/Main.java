47a15a73f468060b4c79a05a76eadc82a6ec40e7
107380www <zhaotian6688@163.com>
2020-11-26 14:11:42 +0800

    233232

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code heres
+        System.out.println(111111);
+        System.out.println(222222);
+    }
+}
